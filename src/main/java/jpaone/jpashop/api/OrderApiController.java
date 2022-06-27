e63ac88bf73ee1a46a3e1e0b16ee1e6bd2426521
LCH <ckcks1997@gmail.com>
2022-06-27 21:33:13 +0900

    인프런 강의2 - API 개발
    - collection 조회 최적화(3)
    
    * V5: order조회 후 id값들 추출, 추출값으로 OrderItem조회

--- src/main/java/jpaone/jpashop/api/OrderApiController.java
+++ src/main/java/jpaone/jpashop/api/OrderApiController.java
@@ -79,6 +79,10 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimizaion();
+    }
 
     @Data
     @AllArgsConstructor
--- src/main/java/jpaone/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpaone/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderQueryRepository {
     private final EntityManager em;
 
-    public List<OrderQueryDto> findOrderQueryDtos(){
+    public List<OrderQueryDto> findOrderQueryDtos(){//v4
         List<OrderQueryDto> result = findOrders();
         result.forEach(o->{
             List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
@@ -20,7 +22,7 @@ public class OrderQueryRepository {
         return result;
     }
 
-    private List<OrderItemQueryDto> findOrderItems(Long orderId) {
+    private List<OrderItemQueryDto> findOrderItems(Long orderId) {//v4
         return em.createQuery("select" +
                         " new jpaone.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi " +
@@ -30,7 +32,7 @@ public class OrderQueryRepository {
                 .getResultList();
     }
 
-    private List<OrderQueryDto> findOrders() {
+    private List<OrderQueryDto> findOrders() {//v4
         return em.createQuery("select " +
                         "new jpaone.jpashop.repository.order.query.OrderQueryDto" +
                         "(o.id,m.name, o.orderDate, o.status, d.address ) " +
@@ -38,5 +40,28 @@ public class OrderQueryRepository {
                         "join o.member m " +
                         "join o.delivery d", OrderQueryDto.class)
                 .getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimizaion() { //v5
+        List<OrderQueryDto> result = findOrders(); //order조회
+
+        List<Long> orderIds = result.stream() //order의 id값들을 리스트에 담아둠
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+
+        List<OrderItemQueryDto> orderItems = em.createQuery("select" + //id값들로 OrderItem 조회
+                        " new jpaone.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " from OrderItem oi " +
+                        "join oi.item i " +
+                        "where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream() //id값들로 값을 묶은 map 생성
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId()))); //위 값 주입
+
+        return result;
     }
 }
